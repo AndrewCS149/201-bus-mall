61f2df54d6fd7502fe2aa729b2b344348e5de7b3
AndrewCS149 <Andrewcs149@gmail.com>
2020-05-12 12:39:50 -0700

    Add horizontal bar chart from chart.js

--- js/app.js
+++ js/app.js
@@ -2,18 +2,15 @@
 
 //TODO: calc percentages of votes to views for each img
 
-// 2d array of imgs and img file paths
-var imgsAndPaths = [
-  ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'],
-
-  ['jpg', 'jpg', 'jpg', 'jpg', 'jpg', 'jpg', 'jpg', 'jpg','jpg', 'jpg', 'jpg', 'jpg', 'jpg', 'jpg', 'png', 'jpg', 'jpg', 'gif', 'jpg', 'jpg']
-];
+var imgTitles = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+var imgExtensions = ['jpg', 'jpg', 'jpg', 'jpg', 'jpg', 'jpg', 'jpg', 'jpg','jpg', 'jpg', 'jpg', 'jpg', 'jpg', 'jpg', 'png', 'jpg', 'jpg', 'gif', 'jpg', 'jpg'];
 
 var imgParent = document.getElementById('imgs');
 var listParent = document.getElementById('vote-results');
 var allImgs = [];
 var rounds = 25;
 var count = 0;
+var votesArr = [];
 
 // constructor function
 function ImgCreator(title, extension) {
@@ -60,6 +57,7 @@ function getRandomImg() {
   if (count === rounds) { //TODO: doesnt add up
     imgParent.removeEventListener('click', getRandomImg);
     ImgCreator.prototype.appendList();
+    generateChart();
     return;
   }
   count++;
@@ -88,6 +86,12 @@ function getRandomImg() {
   allImgs[idx3].views++;
 }
 
+function votesArray() {
+  for(var i = 0; i < allImgs.length; i++){
+    votesArr.push(allImgs[i].votes);
+  }
+}
+
 // event listener to record how many times each image was clicked
 imgParent.addEventListener('click', function () {
   var title = event.target.title;
@@ -106,7 +110,50 @@ imgParent.addEventListener('click', function () {
 
 // create all ImgCreator instances
 for(var i = 0; i < 20; i++) {
-  new ImgCreator(imgsAndPaths[0][i], imgsAndPaths[1][i]);
+  new ImgCreator(imgTitles[i], imgExtensions[i]);
+}
+
+
+// generate chart
+function generateChart() {
+  votesArray();
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: {
+      labels: imgTitles,
+      datasets: [{
+        label: '# of Votes',
+        data: votesArr,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
 }
 
 getRandomImg();
