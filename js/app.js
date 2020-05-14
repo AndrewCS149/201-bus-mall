feea33bd1e2030872d04944e6b01d051a10868df
AndrewCS149 <Andrewcs149@gmail.com>
2020-05-13 18:14:15 -0700

    Create local storage for titles, views and votes

--- js/app.js
+++ js/app.js
@@ -16,6 +16,8 @@ var rgbValues = [];
 var rgbBorders = [];
 var uniqueIdxArr = [];
 var totalVotes = 0;
+var arr = []; // locally stored images
+
 
 // constructor function
 function ImgCreator(title, extension) {
@@ -41,6 +43,18 @@ ImgCreator.prototype.render = function () {
   allImgs[idx2].views++;
   allImgs[idx3].appendImage();
   allImgs[idx3].views++;
+
+  // local storage views
+  var arrLocal = localStorage.getItem('imgs');
+  arr = JSON.parse(arrLocal);
+
+  // add local storage views
+  arr[idx1].views += 1;
+  arr[idx2].views += 1;
+  arr[idx3].views += 1;
+
+  var strArrLocal = JSON.stringify(arr);
+  localStorage.setItem('imgs', strArrLocal);
 };
 
 // create an image element holding the title and url. Append to imgParent.
@@ -69,13 +83,6 @@ ImgCreator.prototype.appendList = function () {
       listItem.textContent = `${allImgs[i].title} had ${allImgs[i].votes} votes and was shown ${allImgs[i].views} times.`;
 
       listParent.appendChild(listItem);
-
-      // // locally store total votes
-      // var votesLocal = localStorage.getItem('votes');
-      // totalVotes = JSON.parse(votesLocal);
-      // totalVotes += allImgs[i].votes;
-      // var stringifiedTotals = JSON.stringify(totalVotes);
-      // localStorage.setItem('votes', stringifiedTotals);
     }
   }
 };
@@ -123,23 +130,11 @@ function generateRGB() {
   }
 }
 
-// locally store total votes
-function storeLocal() {
-  var votesLocal = localStorage.getItem('votes');
-  totalVotes = JSON.parse(votesLocal);
-  totalVotes += 1;
-  var stringifiedTotals = JSON.stringify(totalVotes);
-  localStorage.setItem('votes', stringifiedTotals);
-}
-
 // handler function for click event on #images
 imgParent.addEventListener('click', handleClick);
 function handleClick(event) {
   var title = event.target.title;
   console.log(title);
-
-  // locally store all votes
-  storeLocal();
 
   // loop through all images
   for (var i = 0; i < allImgs.length; i++) {
@@ -148,6 +143,14 @@ function handleClick(event) {
     if (title === allImgs[i].title) {
       allImgs[i].votes++;
       allImgs[i].hasVotes = true;
+
+      var arrLocal = localStorage.getItem('imgs');
+      arr = JSON.parse(arrLocal);
+
+      arr[i].votes++;
+
+      var strArrLocal = JSON.stringify(arr);
+      localStorage.setItem('imgs', strArrLocal);
 
       // // keep track of rounds
       if (count === rounds) {
@@ -162,28 +165,33 @@ function handleClick(event) {
   displayImage();
 }
 
-// create all ImgCreator instances
-for (var i = 0; i < 20; i++) {
-  new ImgCreator(imgTitles[i], imgExtensions[i]);
+// create all ImgCreator instances and store in local storage
+if (localStorage.getItem('imgs') === null){
+  for (var i = 0; i < 20; i++) {
+    new ImgCreator(imgTitles[i], imgExtensions[i]);
+    arr.push({Title: allImgs[i].title, votes: 0, views: 0});
+  }
+  var strArrLocal = JSON.stringify(arr);
+  localStorage.setItem('imgs', strArrLocal);
+} else {
+  for (i = 0; i < 20; i++) {
+    new ImgCreator(imgTitles[i], imgExtensions[i]);
+  }
 }
-
-// Local Storage
-// function storeLocal() {
-
-// }
-
 
 //TODO: make these not generate chart without finishing the voting process
 function darkMode() {
   Chart.defaults.global.defaultFontColor = 'white';
   generateChart();
   document.body.style.backgroundColor = '#424242';
+  document.body.style.color = 'white';
 }
 
 function lightMode() {
   Chart.defaults.global.defaultFontColor = 'black';
   generateChart();
   document.body.style.backgroundColor = 'white';
+  document.body.style.color = 'black';
 }
 
 displayImage();
@@ -223,3 +231,4 @@ function generateChart() {
     }
   });
 }
+
